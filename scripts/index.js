a02c5217d5ff0262d4f3f12af05f1901d952406c
Vera Smirnova <111021785+Fel1da@users.noreply.github.com>
2026-02-17 21:48:06 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, deleteHandler) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  deleteButton.addEventListener('click', () => deleteHandler(cardElement));
+
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, deleteCard);
+  placesList.append(card);
+});
